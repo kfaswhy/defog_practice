c4dd65a2d6b158c84fecb5005666c7bfcd30da0e
Hank Wang <kfaswhy@gmail.com>
2024-08-12 16:19:25 +0800

    初始化可用

--- bmp_process.h
+++ bmp_process.h
@@ -1,29 +1,10 @@
 #ifndef _BMP_PROCESS_H_
 #define _BMP_PROCESS_H_
+
 #include <iostream>
 #include <windows.h> 
 #include "defog.h"
 
-typedef struct
-{
-	BYTE b;
-	BYTE g;
-	BYTE r;
-}RGB;
 
-typedef struct
-{
-	BYTE b;
-	BYTE g;
-	BYTE r;
-	BYTE a;
-}RGBA;
-
-
-RGB* load_bmp(const char* filename);
-
-int img_process(RGB* img);
-
-void save_bmp(const char* filename, RGB* img);
 
 #endif
--- defog.h
+++ defog.h
@@ -4,5 +4,26 @@
 #include <iostream>
 #include <windows.h> 
 #include "bmp_process.h"
+typedef struct _RGB
+{
+	BYTE b;
+	BYTE g;
+	BYTE r;
+}RGB;
+
+typedef struct
+{
+	BYTE b;
+	BYTE g;
+	BYTE r;
+	BYTE a;
+}RGBA;
+
+
+RGB* load_bmp(const char* filename);
+
+void save_bmp(const char* filename, RGB* img);
+
+int img_process(RGB* img);
 
 #endif
